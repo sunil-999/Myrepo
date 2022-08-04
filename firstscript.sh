86816e4b82185e5d04333d0bb9c520e10cedcce8
sunil-999 <96425122+sunil-999@users.noreply.github.com>
2022-08-04 18:30:32 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -1,4 +1,4 @@
-mkdir /home/vagrant/shelli=_scripts
+mkdir /home/vagrant/shellscripts
 touch /home/vagrant/shell_scripts/shell.txt
 cat > /home/vagrant/shell_scripts/shell.txt <<EOF
 this is to test my initial shell script
