9eb9232c73f00f3bb3d6f897ca8bae664cbc3d5b
sunil-999 <96425122+sunil-999@users.noreply.github.com>
2022-08-04 18:34:27 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -1,5 +1,7 @@
+rm -rf /home/vagrant/shell_scripts
 mkdir /home/vagrant/shell_scripts
 touch /home/vagrant/shell_scripts/shell.txt
 cat > /home/vagrant/shell_scripts/shell.txt <<EOF
 this is to test my initial shell script
+This is to test from jenkins build byd doing some changes in script
 EOF
