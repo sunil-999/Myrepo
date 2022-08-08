6ee7c81c34b8c2120e30d06a58099f56efa7060f
sunil-999 <96425122+sunil-999@users.noreply.github.com>
2022-08-08 17:47:04 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -5,3 +5,13 @@ cat > /home/vagrant/shell_scripts/shell.txt <<EOF
 this is to test my initial shell script
 This is to test from jenkins build byd doing some changes in script
 EOF
+echo "Enter the value:"
+read count
+if [ $count -eq 10 ]
+then
+   echo "Values are equal"
+elif [[ a == b ]]
+then "condition is true"
+else 
+   echo "condition is false"
+fi 
